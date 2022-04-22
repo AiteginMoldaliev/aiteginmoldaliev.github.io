52cc5ffd572e2849427412d887dde51cf03381d0
AiteginMoldaliev <ayte14@mail.ru>
2022-04-22 11:26:48 +0600

    'portfolio'

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainpage.component.scss']
 })
 export class MainpageComponent implements OnInit {
+  portfolio:any =[1,2,3,4,5,6];
+
 
   constructor() { }
 
