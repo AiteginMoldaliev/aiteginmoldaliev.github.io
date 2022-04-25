af6747ab9ffcb022e96f4b33af0fbd9db5d60a68
AiteginMoldaliev <ayte14@mail.ru>
2022-04-25 16:56:21 +0600

    'experience'

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -6,12 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mainpage.component.scss']
 })
 export class MainpageComponent implements OnInit {
-  portfolio:any =[1,2,3,4,5,6];
-
-
-  constructor() { }
-
-  ngOnInit(): void {
+  pullOn:any = '';
+  exit:any = 'fa-solid fa-bars';
+  experinces:any = [
+    {name: 'HTML&CSS', style:['background-color:white', 'width:70%']},
+    {name: 'Bootstrap', style:['background-color:white','width:70%']},
+    {name: 'SCSS', style:['background-color:white','width:70%']},
+    {name: 'JS', style:['background-color:white','width:70%']},
+    {name: 'Git', style:['background-color:white','width:70%']},
+    {name: 'JQuery', style:['background-color:white','width:70%']},
+    {name: 'Phython', style:['background-color:white','width:70%']},
+    {name: 'TypeScript', style:['background-color:white','width:70%']},
+    {name: 'WordPress', style:['background-color:white','width:70%']},
+    {name: 'PHP', style:['background-color:white','width:70%']},
+    {name: 'SQL', style:['background-color:white','width:70%']},
+    {name: 'ReactJS', style:['background-color:white','width:70%']}
+  ]
+  constructor() { 
   }
 
+  ngOnInit(): void {
+    
+  }
+  pullR(){
+    if(this.pullOn === ''){
+    this.pullOn='pullOn';
+    }else{
+    this.pullOn='';
+    }
+    if(this.exit === 'fa-solid fa-bars'){
+    this.exit='fa-solid fa-xmark';
+    }else{ 
+    this.exit='fa-solid fa-bars';
+    }
+  }
 }
